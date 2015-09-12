512b1167037cd801b84e764c3f885c984bef6c20
Sayed Hadi Hashemi <public@xldrx.com>
2015-09-12 18:48:21 -0500

    Add examples

---
+++ tutorial/RetrieveData.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class RetrieveData{
+
+   public static void main(String[] args) throws IOException, Exception{
+
+      // Instantiating Configuration class
+      Configuration config = HBaseConfiguration.create();
+
+      // Instantiating HTable class
+      HTable table = new HTable(config, "emp");
+
+      // Instantiating Get class
+      Get g = new Get(Bytes.toBytes("row1"));
+
+      // Reading the data
+      Result result = table.get(g);
+
+      // Reading values from Result class object
+      byte [] value = result.getValue(Bytes.toBytes("personal"),Bytes.toBytes("name"));
+
+      byte [] value1 = result.getValue(Bytes.toBytes("personal"),Bytes.toBytes("city"));
+
+      // Printing the values
+      String name = Bytes.toString(value);
+      String city = Bytes.toString(value1);
+
+      System.out.println("name: " + name + " city: " + city);
+   }
+}
