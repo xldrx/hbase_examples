24e7b0bfc5524171c2294f1ce9a7b1198051ca9b
Sylvain Leroux <sylvain@chicoree.fr>
2015-09-15 23:49:18 +0200

    Fixed use of methods deprecated in HBase 1.0.0

--- tutorial/CreateTable.java
+++ tutorial/CreateTable.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.TableName;
 
 import org.apache.hadoop.conf.Configuration;
@@ -15,19 +17,20 @@ public class CreateTable {
       // Instantiating configuration class
       Configuration con = HBaseConfiguration.create();
 
-      // Instantiating HbaseAdmin class
-      HBaseAdmin admin = new HBaseAdmin(con);
+      try (Connection connection = ConnectionFactory.createConnection(con); 
+           Admin admin = connection.getAdmin()) {
 
-      // Instantiating table descriptor class
-      HTableDescriptor tableDescriptor = new
-      HTableDescriptor(TableName.valueOf("emp"));
+          // Instantiating table descriptor class
+          HTableDescriptor tableDescriptor = new
+          HTableDescriptor(TableName.valueOf("emp"));
 
-      // Adding column families to table descriptor
-      tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-      tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+          // Adding column families to table descriptor
+          tableDescriptor.addFamily(new HColumnDescriptor("personal"));
+          tableDescriptor.addFamily(new HColumnDescriptor("professional"));
 
-      // Execute the table through admin
-      admin.createTable(tableDescriptor);
-      System.out.println(" Table created ");
+          // Execute the table through admin
+          admin.createTable(tableDescriptor);
+          System.out.println(" Table created ");
+      }
    }
 }
--- tutorial/ScanTable.java
+++ tutorial/ScanTable.java
@@ -5,7 +5,10 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.util.Bytes;
 
-import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
@@ -17,25 +20,24 @@ public class ScanTable{
 
       // Instantiating Configuration class
       Configuration config = HBaseConfiguration.create();
+      try (Connection conn = ConnectionFactory.createConnection(config);
+           Table table = conn.getTable(TableName.valueOf("emp"))) {
 
-      // Instantiating HTable class
-      HTable table = new HTable(config, "emp");
+          // Instantiating the Scan class
+          Scan scan = new Scan();
 
-      // Instantiating the Scan class
-      Scan scan = new Scan();
+          // Scanning the required columns
+          scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name"));
+          scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("city"));
 
-      // Scanning the required columns
-      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name"));
-      scan.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("city"));
+          // Getting the scan result
+          try (ResultScanner scanner = table.getScanner(scan)) {
 
-      // Getting the scan result
-      ResultScanner scanner = table.getScanner(scan);
+              // Reading values from scan result
+              for (Result result = scanner.next(); result != null; result = scanner.next())
 
-      // Reading values from scan result
-      for (Result result = scanner.next(); result != null; result = scanner.next())
-
-      System.out.println("Found row : " + result);
-      //closing the scanner
-      scanner.close();
+              System.out.println("Found row : " + result);
+          }
+      }
    }
 }
